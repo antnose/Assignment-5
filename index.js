b5bb82f85c015dd4a957e67a50701090be91bdf0
antnose <antnose@gmail.com>
2025-08-27 00:46:20 +0600

    complete the website

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const lifeCount = document.querySelector("#lifeCount");
 const coin = document.querySelector("#coinCount");
+let historySection = document.querySelector("#historySection");
 let life = 0;
 let coinValue = 100;
 document.querySelectorAll(".love_btn").forEach((btn) => {
@@ -18,9 +19,31 @@ document.querySelectorAll(".call_btn").forEach((btn) => {
     if (coinValue >= 20) {
       if (confirm(`Calling ${cardTitle} ${cardNumber} `)) {
         coin.innerText = coinValue -= 20;
+        const date = new Date();
+        historySection.innerHTML += `
+           <div
+          class="flex items-center justify-between bg-gray-300 p-3 rounded-xl mt-3"
+        >
+          <div>
+            <p>${cardTitle}</p>
+            <p>${cardNumber}</p>
+          </div>
+
+          <div>
+          ${date.toLocaleTimeString()}
+          </div>
+        </div>
+      </div>
+    </div>
+        `;
       } else return;
     } else {
       alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
     }
   });
+});
+
+document.querySelector("#historyClearBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+  historySection.innerHTML = "";
 });
