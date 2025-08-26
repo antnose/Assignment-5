3aeffede1dc7521356b46e631e561472da02e9c9
antnose <antnose@gmail.com>
2025-08-26 23:59:13 +0600

    Make coin section functional

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ document.querySelectorAll(".love_btn").forEach((btn) => {
     lifeCount.innerHTML = ++life;
   });
 });
+document.querySelectorAll(".call_btn").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const fullCard = btn.closest(".card");
+    const cardTitle = fullCard.querySelector(".card-title").innerText;
+    const cardNumber = fullCard.querySelector(".number").innerText;
+
+    e.preventDefault();
+    if (coinValue >= 20) {
+      if (confirm(`Calling ${cardTitle} ${cardNumber} `)) {
+        coin.innerText = coinValue -= 20;
+      } else return;
+    } else {
+      alert("❌ আপনার পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন লাগবে।");
+    }
+  });
+});
